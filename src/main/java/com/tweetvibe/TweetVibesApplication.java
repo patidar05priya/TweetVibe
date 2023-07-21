d0f382064cc5054958f0179b2a486239db4834d8
PRIYA PATIDAR <patidar05priya@gmail.com>
2023-07-21 15:34:11 -0400

    first commit

---
+++ src/main/java/com/tweetvibe/TweetVibesApplication.java
@@ -0,0 +1,13 @@
+package com.tweetvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetVibesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetVibesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweetvibe/TweetVibesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweetvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetVibesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
